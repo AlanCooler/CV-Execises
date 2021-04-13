d2d8be3db28f22406e25166006a51c5e735d6edb
demonizze <artem@securityarea.com>
2021-04-13 23:12:42 +0300

    Creating basic shape interface

---
+++ Source/Libs/IShape.cs
@@ -0,0 +1,12 @@
+namespace Source.Libs
+{
+    public interface IShape
+    {
+        
+        /// <summary>
+        /// Метод который считает площадь фигуры
+        /// </summary>
+        /// <returns>Площадь фигуры</returns>
+         double Area();
+    }
+}
